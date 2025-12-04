044d61ca2ce77ff995f6a62905ff74f0d6c3f371
mohittiwari34 <152634529+mohittiwari34@users.noreply.github.com>
2025-12-04 22:26:29 +0530

    Create 224-BasicCalculator.cpp

---
+++ LeetCode/224-BasicCalculator.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int calculate(string s) {
+       long long result = 0;
+        long long num = 0;
+        int sign = 1;
+        stack<long long> st;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');   // long long prevents overflow
+            }
+            else if (c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            }
+            else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            }
+            else if (c == '(') {
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if (c == ')') {
+                result += sign * num;
+                num = 0;
+
+                long long prevSign = st.top(); st.pop();
+                long long prevResult = st.top(); st.pop();
+
+                result = prevResult + prevSign * result;
+            }
+        }
+
+        result += sign * num;
+        return (int)result;  
+    }
+};
