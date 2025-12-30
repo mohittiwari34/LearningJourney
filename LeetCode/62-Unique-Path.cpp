a695cf8c2e709d4096c42fef2e323940940352b7
Mohit Tiwari <152634529+mohittiwari34@users.noreply.github.com>
2025-12-30 16:36:25 +0530

    Create 62-Unique-Path.cpp

---
+++ LeetCode/62-Unique-Path.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        //vector<vector<int>>dp(m,vector<int>(n,1));
+        vector<int>dp(n,1);
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[j]+=dp[j+1];
+            }
+        }
+        return dp[0];
+    }
+};
