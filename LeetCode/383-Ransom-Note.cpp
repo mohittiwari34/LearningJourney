f950e440644e610ca919e1109039c8c5300e22ab
Mohit Tiwari <152634529+mohittiwari34@users.noreply.github.com>
2026-01-05 15:18:47 +0530

    Create 383-Ransom-Note.cpp

---
+++ LeetCode/383-Ransom-Note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>m;
+        for(int i=0;i<magazine.size();i++){
+            m[magazine[i]]++;
+        }
+        for(int i=0;i<ransomNote.size();i++){
+            if(m[ransomNote[i]]){
+                m[ransomNote[i]]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
