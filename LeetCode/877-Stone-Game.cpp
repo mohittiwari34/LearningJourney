b43cbcba8f213b767106c45a1450bbfcf4d69421
Mohit Tiwari <152634529+mohittiwari34@users.noreply.github.com>
2026-01-18 22:14:22 +0530

    Create 877-Stone-Game.cpp

---
+++ LeetCode/877-Stone-Game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // bool find(vector<int>& piles,int turn,int i,int n){
+    //     if(i==n){
+    //         return 0;
+    //     }
+
+    // }
+    bool stoneGame(vector<int>& piles) {
+        //int n=piles.size();
+        //return find(piles,0,0,n);
+        //kahi se start kare chahe first se ya last se jitega to alice hi
+        return true;
+    }
+};
