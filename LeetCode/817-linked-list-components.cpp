f49df40bab603b80520f8d7b82d6a9bac3fc7c97
Mohit Tiwari <152634529+mohittiwari34@users.noreply.github.com>
2026-02-16 15:08:32 +0530

    Create 817-linked-list-components.cpp

---
+++ LeetCode/817-linked-list-components.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        int total=0;
+        while(head!=nullptr){
+            if(m[head->val] &&(head->next==nullptr|| m[head->next->val]==0)){
+                total++;
+            }
+            head=head->next;
+        }
+        return total;
+    }
+};
