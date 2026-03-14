ae486d52d11f3daeba0403da1261f9bf33dfae88
Mohit Tiwari <152634529+mohittiwari34@users.noreply.github.com>
2026-03-14 23:16:32 +0530

    Create 39-Combination-Sum.cpp

---
+++ LeetCode/39-Combination-Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(int i, vector<int>& candidates, int target, 
+               vector<int>& temp, vector<vector<int>>& ans) {
+        
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+
+        if(i >= candidates.size() || target < 0)
+            return;
+
+        
+        temp.push_back(candidates[i]);
+        solve(i, candidates, target - candidates[i], temp, ans);
+
+        
+        temp.pop_back();
+        solve(i + 1, candidates, target, temp, ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        solve(0, candidates, target, temp, ans);
+        return ans;
+
+    }
+};
